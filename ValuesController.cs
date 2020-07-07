d269ba2a81ad82f49b0f2344b05cf0919cdd6cce
nehamehta2 <66009563+nehamehta2@users.noreply.github.com>
2020-07-07 22:59:14 +0530

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TestTransPerfect.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string BaseUrl = "http://api.qrserver.com/v1/read-qr-code/";
+        // GET api/values
+        public HttpResponseMessage Post()
+        {
+            
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);                   
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+    }
+}
